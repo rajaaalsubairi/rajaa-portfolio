ccc629ed55e2756560e8a745a3d2b840fe7d16e6
rajaaalsubairi <rajaaal-subairi@Rajaa.local>
2016-05-04 20:04:44 -0400

    Revert "added intro mask and changed gallery style"
    This reverts commit 8a615e92a2fa074294641ed43649899cc49ca7af.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-$(document).ready(function(){       
-    var scroll_pos = 0;
-    $(document).scroll(function() { 
-        scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 20) {
-            $("div").removeClass().addClass('nav-white');
-        } else {
-            $("div").removeClass('nav-white').addClass('nav');
-        }
-    });
-});
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
